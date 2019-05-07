ba79ceed4d955024112dc4f1ca839a3f2d71d081
RishabhBrajabasi <rishabhbrajabasi@gmail.com>
2019-05-07 00:33:26 -0400

    V4

--- app/src/main/java/com/example/ishaara/particle.java
+++ app/src/main/java/com/example/ishaara/particle.java
@@ -2,6 +2,7 @@ package com.example.ishaara;
 
 import android.accessibilityservice.AccessibilityService;
 import android.accessibilityservice.AccessibilityServiceInfo;
+import android.annotation.TargetApi;
 import android.app.ActivityManager;
 import android.app.Service;
 import android.content.ComponentName;
@@ -13,13 +14,16 @@ import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Debug;
 import android.os.IBinder;
+import android.speech.tts.TextToSpeech;
 import android.support.annotation.NonNull;
+import android.support.annotation.RequiresApi;
 import android.util.Log;
 import android.view.accessibility.AccessibilityEvent;
 import android.view.accessibility.AccessibilityNodeInfo;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Locale;
 
 import io.particle.android.sdk.cloud.ParticleCloud;
 import io.particle.android.sdk.cloud.ParticleCloudException;
@@ -39,7 +43,9 @@ public class particle extends AccessibilityService {
     private long subscriptionId;
     private ParticleDevice mDevice;
     private String foreApp = "";
-
+    private  boolean no_gesture = true;
+    private TextToSpeech t1;
+    String notificationContent="";
 
     @Override
     protected void onServiceConnected() {
@@ -47,85 +53,189 @@ public class particle extends AccessibilityService {
 
         //Configure these here for compatibility with API 13 and below.
         AccessibilityServiceInfo config = new AccessibilityServiceInfo();
-        config.eventTypes = AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED;
-        config.feedbackType = AccessibilityServiceInfo.FEEDBACK_GENERIC;
 
-        if (Build.VERSION.SDK_INT >= 16)
+        config.eventTypes = AccessibilityEvent.TYPE_NOTIFICATION_STATE_CHANGED;
+        config.eventTypes = AccessibilityEvent.TYPES_ALL_MASK;
+        config.feedbackType = AccessibilityServiceInfo.FEEDBACK_ALL_MASK;
+        config.notificationTimeout = 100;
+        config.packageNames = null;
+
+        t1=new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if(status != TextToSpeech.ERROR) {
+                    t1.setLanguage(Locale.UK);
+                }
+            }
+        });
+        //        if (Build.VERSION.SDK_INT >= 16)
             //Just in case this helps
-            config.flags = AccessibilityServiceInfo.FLAG_INCLUDE_NOT_IMPORTANT_VIEWS;
+//            config.flags = AccessibilityServiceInfo.FLAG_INCLUDE_NOT_IMPORTANT_VIEWS;
 
         setServiceInfo(config);
     }
 
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
     @Override
     public void onAccessibilityEvent(AccessibilityEvent event) {
-
-        int eventType = event.getEventType();
-
-
-        switch(eventType){
-
-            case AccessibilityEvent.TYPE_WINDOW_CONTENT_CHANGED:
-                Log.i("BANANA", "TYPE_WINDOW_CONTENT_CHANGED");
-                if (event.getPackageName() != null && event.getClassName() != null) {
-                    ComponentName componentName = new ComponentName(
-                            event.getPackageName().toString(),
-                            event.getClassName().toString()
-                    );
-
-                    ActivityInfo activityInfo = tryGetActivity(componentName);
-                    boolean isActivity = activityInfo != null;
-                    if (isActivity)
-                        Log.i("BANANA", "1"+componentName.flattenToShortString());
-                    foreApp = componentName.flattenToShortString();
-                }
-                break;
-            case AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED:
-                Log.i("BANANA", "TYPE_WINDOW_STATE_CHANGED");
-                if (event.getPackageName() != null && event.getClassName() != null) {
-                    Log.d("BANANA", "PCK NAME"+event.getPackageName().toString());
-                    Log.d("BANANA", "CLS NAME"+event.getClassName().toString());
-
-                    ComponentName componentName = new ComponentName(
-                            event.getPackageName().toString(),
-                            event.getClassName().toString()
-
-                    );
-
-                    ActivityInfo activityInfo = tryGetActivity(componentName);
-                    boolean isActivity = activityInfo != null;
-                    if (isActivity)
-                        Log.i("BANANA", "2"+componentName.flattenToShortString());
-                    foreApp = componentName.flattenToShortString();
-                }
-                break;
-            case AccessibilityEvent.TYPE_NOTIFICATION_STATE_CHANGED:
-                Log.i("BANANA", "TYPE_NOTIFICATION_STATE_CHANGED");
-                if (event.getPackageName() != null && event.getClassName() != null) {
-                    ComponentName componentName = new ComponentName(
-                            event.getPackageName().toString(),
-                            event.getClassName().toString()
-                    );
-
-                    ActivityInfo activityInfo = tryGetActivity(componentName);
-                    boolean isActivity = activityInfo != null;
-                    if (isActivity)
-                        Log.i("BANANA", "3"+componentName.flattenToShortString());
-                    foreApp = componentName.flattenToShortString();
-                }
-                //for (String id : installBtnId) {
-//                    AccessibilityNodeInfo node = AccessibilityNodeInfo.
-//                            AccessibilityNodeUtil.findNodeById(event.getSource(), id);
-//                    if (node != null) {
-//                        AccessibilityNodeUtil.click(node);
-//                        Log.d("BANANA", "Some accessibility event occured");
-//                        break;
-//                    }
-//                //}
-                break;
-            default:
-                break;
+        if(no_gesture == true){
+            //Log.d("BANANA", "returning early from Accessibility Event");
+            //return;
         }
+        int eventType = event.getEventType();
+        AccessibilityNodeInfo currentNode=getRootInActiveWindow();
+        if(event.getEventType()==AccessibilityEvent.TYPE_VIEW_CLICKED){
+            Log.d("BANANA", "Content Clicked");
+        }
+
+
+        if (currentNode!=null && event.getEventType()==AccessibilityEvent.TYPE_WINDOW_CONTENT_CHANGED){
+            if (event.getPackageName().equals("com.spotify.music")) {
+                Log.d("BANANA","PACKAGE NAME : "+event.getPackageName().toString());
+                Log.d  ("BANANA", "ClassName: "+ currentNode.getClassName());
+                int childCnt = currentNode.getChildCount();
+                for(int i = 0; i < childCnt; i++){
+                    Log.d("BANANA", "ChildNum "+i+"Child Name "+currentNode.getChild(i).getContentDescription());
+                    //List<AccessibilityNodeInfo.AccessibilityAction> actionList= currentNode.getChild(i).getActionList();
+                    //Log.d("BANANA", "ChildNum "+i+"Action List "+actionList);
+                    //currentNode.getChild(i).performAction(AccessibilityNodeInfo.ACTION_CLICK);
+                }
+            }
+
+            if (event.getPackageName().equals("com.android.dialer")) {
+
+                Log.d("BANANA", "Window Accessed "+currentNode);
+                Log.d  ("BANANA", "ClassName: "+ currentNode.getClassName());
+                int childCnt = currentNode.getChildCount();
+                for(int i = 0; i < childCnt; i++){
+                    Log.d("BANANA", "ChildNum "+i+"Child Name "+currentNode.getChild(i).isFocusable());
+                    List<AccessibilityNodeInfo.AccessibilityAction> actionList= currentNode.getChild(i).getActionList();
+                    Log.d("BANANA", "ChildNum "+i+"Action List "+actionList);
+                    currentNode.getChild(i).performAction(AccessibilityNodeInfo.ACTION_CLICK);
+                }
+//                if (currentNode!=null && currentNode.getClassName().equals("android.widget.FrameLayout") &&
+// currentNode.getChild(2)!=null && currentNode.getChild(2).getClassName().equals("android.widget.TextView") &&
+// currentNode.getChild(2).getContentDescription().equals("Search")) {
+//                    Log.d("BANANA", "Clicked Something");
+//                    currentNode.getChild(2).performAction(AccessibilityNodeInfo.ACTION_CLICK);
+//                }
+            }
+        }
+
+        if(currentNode!=null && eventType == AccessibilityEvent.TYPE_NOTIFICATION_STATE_CHANGED){
+            Log.d("BANANA","PACKAGE NAME : "+event.getPackageName().toString());
+
+            if (currentNode!=null && event.getPackageName().toString().equals("com.android.dialer")){
+
+                //AccessibilityNodeInfo currentNode=getRootInActiveWindow();
+                Log.d("BANANA", "Dialer NODE:"+currentNode);
+
+                StringBuilder message = new StringBuilder();
+                if (!event.getText().isEmpty()) {
+                    for (CharSequence subText : event.getText()) {
+                        message.append(subText);
+                    }
+                    String name="Pallavi";
+                    if (message.toString().contains("Message from")){
+                        name=message.toString().substring(13);
+                    }
+                    Log.d("BANANA","M : "+message);
+                }
+            }
+
+            if (event.getPackageName().toString().equals("com.whatsapp")){
+
+                //AccessibilityNodeInfo currentNode=getRootInActiveWindow();
+                Log.d("BANANA", "NODE:"+currentNode);
+                Log.d("BANANA","PACKAGE NAME : "+event.getPackageName().toString());
+                Log.d  ("BANANA", "ClassName: "+ currentNode.getClassName());
+                int childCnt = currentNode.getChildCount();
+                for(int i = 0; i < childCnt; i++){
+                    Log.d("BANANA", "ChildNum "+i+"Child Name "+currentNode.getChild(i).getContentDescription());
+                    //List<AccessibilityNodeInfo.AccessibilityAction> actionList= currentNode.getChild(i).getActionList();
+                    //Log.d("BANANA", "ChildNum "+i+"Action List "+actionList);
+                    //currentNode.getChild(i).performAction(AccessibilityNodeInfo.ACTION_CLICK);
+                }
+                StringBuilder message = new StringBuilder();
+                if (!event.getText().isEmpty()) {
+                    for (CharSequence subText : event.getText()) {
+                        message.append(subText);
+                    }
+                    if (message.toString().contains("Message from")){
+                        String name=message.toString().substring(13);
+                        Log.d("BANANA","NAME : "+name);
+                    }
+                    Log.d("BANANA","M : "+message);
+                    notificationContent = notificationContent+ message.toString();
+                }
+            }
+        }
+
+
+//        switch(eventType){
+//
+//            case AccessibilityEvent.TYPE_WINDOW_CONTENT_CHANGED:
+//                Log.i("BANANA", "TYPE_WINDOW_CONTENT_CHANGED");
+//                if (event.getPackageName() != null && event.getClassName() != null) {
+//                    ComponentName componentName = new ComponentName(
+//                            event.getPackageName().toString(),
+//                            event.getClassName().toString()
+//                    );
+//
+//                    ActivityInfo activityInfo = tryGetActivity(componentName);
+//                    boolean isActivity = activityInfo != null;
+//                    if (isActivity)
+//                        Log.i("BANANA", "1"+componentName.flattenToShortString());
+//                    foreApp = componentName.flattenToShortString();
+//                }
+//                break;
+//            case AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED:
+//                Log.i("BANANA", "TYPE_WINDOW_STATE_CHANGED");
+//                if (event.getPackageName() != null && event.getClassName() != null) {
+//                    Log.d("BANANA", "PCK NAME"+event.getPackageName().toString());
+//                    Log.d("BANANA", "CLS NAME"+event.getClassName().toString());
+//
+//                    ComponentName componentName = new ComponentName(
+//                            event.getPackageName().toString(),
+//                            event.getClassName().toString()
+//
+//                    );
+//
+//                    ActivityInfo activityInfo = tryGetActivity(componentName);
+//                    boolean isActivity = activityInfo != null;
+//                    if (isActivity)
+//                        Log.i("BANANA", "2"+componentName.flattenToShortString());
+//                    foreApp = componentName.flattenToShortString();
+//                }
+//                break;
+//            case AccessibilityEvent.TYPE_NOTIFICATION_STATE_CHANGED:
+//                Log.i("BANANA", "TYPE_NOTIFICATION_STATE_CHANGED");
+//                if (event.getPackageName() != null && event.getClassName() != null) {
+//                    ComponentName componentName = new ComponentName(
+//                            event.getPackageName().toString(),
+//                            event.getClassName().toString()
+//                    );
+//
+//                    ActivityInfo activityInfo = tryGetActivity(componentName);
+//                    boolean isActivity = activityInfo != null;
+//                    if (isActivity)
+//                        Log.i("BANANA", "3"+componentName.flattenToShortString());
+//                    foreApp = componentName.flattenToShortString();
+//                }
+//                //for (String id : installBtnId) {
+////                    AccessibilityNodeInfo node = AccessibilityNodeInfo.
+////                            AccessibilityNodeUtil.findNodeById(event.getSource(), id);
+////                    if (node != null) {
+////                        AccessibilityNodeUtil.click(node);
+////                        Log.d("BANANA", "Some accessibility event occured");
+////                        break;
+////                    }
+////                //}
+//                break;
+//            default:
+//                break;
+//        }
 //        if (event.getEventType() == AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED) {
 //            if (event.getPackageName() != null && event.getClassName() != null) {
 //                ComponentName componentName = new ComponentName(
@@ -140,6 +250,7 @@ public class particle extends AccessibilityService {
 //                    foreApp = componentName.flattenToShortString();
 //            }
 //        }
+        no_gesture = false;
     }
 
     private ActivityInfo tryGetActivity(ComponentName componentName) {
@@ -204,9 +315,20 @@ public class particle extends AccessibilityService {
                             null,  // the first argument, "eventNamePrefix", is optional
                             new ParticleEventHandler() {
                                 public void onEvent(String eventName, ParticleEvent event) {
-                                    Log.i("BANANA", "Received event with payload: " + event.dataPayload);
-                                    if(foreApp.equalsIgnoreCase(getString(R.string.youTube))){
-
+                                    Log.i("BANANA", "Received event " +eventName+" with payload: " + event.dataPayload);
+                                    if(eventName.equals("GestureDetected")) {
+                                        String hello = "Hello";
+                                        t1.speak(notificationContent, TextToSpeech.QUEUE_FLUSH, null);
+                                                /*                                        b1.setOnClickListener(new View.OnClickListener() {
+                                            @Override
+                                            public void onClick(View v) {
+                                                String toSpeak = ed1.getText().toString();
+                                                Toast.makeText(getApplicationContext(), toSpeak,Toast.LENGTH_SHORT).show();
+                                                t1.speak(toSpeak, TextToSpeech.QUEUE_FLUSH, null);
+                                            }
+                                        });*/
+                                        Log.i("BANANA", "Got required event " +eventName+" with payload: " + event.dataPayload);
+                                        no_gesture = false;
                                     }
                                 }
 
